5bcf26c7f2bd5b806e57e0141cda99a488e76cc0
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-10-15 23:29:41 +0300

    this add the authentication systems and the models

---
+++ authentication/models.py
@@ -0,0 +1,62 @@
+
+  
+from django.db import models
+
+
+from django.contrib.auth.models import(
+    AbstractBaseUser,BaseUserManager,PermissionsMixin)
+from rest_framework_simplejwt.tokens import RefreshToken
+
+class UserManager(BaseUserManager):
+    def create_user(self,username,email,password=None):
+        if username is None:
+            raise TypeError('User should have a username')
+        if email is None:
+            raise TypeError('User should have an Email')
+        user=self.model(username=username,email=self.normalize_email(email))
+        user.set_password(password)
+        user.save()
+        return user
+        
+    def create_superuser(self,username,email,password=None):
+        if password is None:
+            raise TypeError('Password should not be none')
+        user=self.create_user(username,email,password)
+        user.is_superuser=True
+        user.is_staff=True
+        user.save()
+        return user
+
+class User(AbstractBaseUser,PermissionsMixin):
+    """
+    An abstract base class implementing a fully featured User model with
+    admin-compliant permissions.
+    Username and password are required. Other fields are optional.
+    """
+    username = models.CharField(max_length=255,unique=True,db_index=True ) 
+    email = models.EmailField(max_length=255,unique=True,db_index=True )
+    is_verified = models.BooleanField(default=True)
+    is_active = models.BooleanField(default = True)
+    created_at =models.DateTimeField(auto_now_add =True)
+    updated_at = models.DateTimeField(auto_now =True)
+    is_staff = models.BooleanField(
+        ('staff status'),
+        default=False,
+        help_text=('Designates whether the user can log into this admin site.'),
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects  = UserManager()   
+
+    def __str__(self):
+        return self.email
+
+
+    def tokens(self):
+        refresh = RefreshToken.for_user(self)
+        return {
+            'refresh':str(refresh),
+            'access':str(refresh.access_token)
+        }
---
+++ schema/serializers.py
@@ -0,0 +1,53 @@
+
+  
+from rest_framework import serializers
+from .models import *
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('title', 'description','image', 'posted_on', 'user')
+        model = Post
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields =('customer','address','contact','orders','total_sale')
+        model = Customer
+
+class FoodSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields =('name', 'course', 'status', 'price', 'image', 'num_order', 'content_description', 'location')
+        model = Food
+
+class StaffSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields =('staff_id', 'address', 'contact', 'salary', 'role')
+        model = Staff
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields =('customer', 'order_timestamp', 'delivery_timestamp', 'payment_status', 'delivery_status', 'if_cancelled', 'total_amount', 'payment_method', 'location', 'delivery_boy')
+        model = Order
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields =('user','content')
+        model = Comment
+
+class DataSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields =('date','sales','expenses')
+        model = Data
+
+class OrderContentSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields =('food','order')
+        model = OrderContent
+
+class CartSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields =('food','user')
+        model = Cart
+
+class DeliveryBoySerializer(serializers.ModelSerializer):
+    class Meta:
+        fields =('order','delivery_boy')
+        model = DeliveryBoy  
