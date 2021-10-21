9a1411c8a4b99395c48ddb1a571247a10c493536
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-10-21 09:33:22 +0300

    deploy to heroku

--- schema/models.py
+++ schema/models.py
@@ -21,7 +21,7 @@ class Customer(models.Model):
     total_sale = models.IntegerField(default=0)
 
     def __str__(self):
-       return self.customer
+      return self.customer.__str__()
 
 class Staff(models.Model):
     admin = 'Admin'
@@ -42,7 +42,7 @@ class Staff(models.Model):
     role = models.CharField(max_length = 30, choices = ROLES)
     
     def __str__(self):
-        return self.email
+        return self.staff_id
 
 class Order(models.Model):
     pending = 'Pending'
