169b73664de4fb9020f2bda4af975a905e9eb96e
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-10-21 11:25:32 +0300

    deploy to heroku

--- schema/models.py
+++ schema/models.py
@@ -14,6 +14,7 @@ class Customer(models.Model):
         (pending,pending),
         (verified,verified),
     )
+    id = models.AutoField(primary_key=True)
     customer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     address = models.TextField()
     contact = models.CharField(max_length = 10)
