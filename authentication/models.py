36207b5940848a519b9b448949c5930abcb3800c
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-10-22 15:02:28 +0300

    deploy to heroku

--- authentication/models.py
+++ authentication/models.py
@@ -34,6 +34,7 @@ class User(AbstractBaseUser,PermissionsMixin):
     Username and password are required. Other fields are optional.
     """
     id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=255)
     username = models.CharField(max_length=255,unique=True,db_index=True ) 
     email = models.EmailField(max_length=255,unique=True,db_index=True )
     is_verified = models.BooleanField(default=True)
